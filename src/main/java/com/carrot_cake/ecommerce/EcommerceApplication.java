b330b0bd3969066f98b193056acaf25da6bf00fb
Kimani <KEN22934@kcbgroup.com>
2024-05-23 10:48:13 +0300

    initial commit

---
+++ src/main/java/com/carrot_cake/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.carrot_cake.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
