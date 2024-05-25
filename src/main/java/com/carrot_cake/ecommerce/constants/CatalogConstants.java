b8fe1b3fa65f8bf06a5d2a7b2d3486ab26875c50
GitHUBProject26 <eugenemaz707@gmail.com>
2024-05-25 09:44:58 +0300

    Add files via upload

---
+++ src/main/java/com/carrot_cake/ecommerce/constants/CatalogConstants.java
@@ -0,0 +1,27 @@
+package com.carrot_cake.ecommerce.constants;
+
+public final class CatalogConstants
+{
+    public static final String STATUS_201 = new String();
+    public static final String MESSAGE_201 = new String();
+    public static final String STATUS_200 = new String();
+    public static final String MESSAGE_200 = new String();
+    public static final String STATUS_417 = new String();
+    public static final String MESSAGE_417_UPDATE = new String();
+    public static final String MESSAGE_417_DELETE = new String();
+
+    private CatalogConstants()
+    {
+    }
+
+    public static final String CATALOG = "Catalogs";
+    public static final String  DESCRIPTION = "Description";
+    private static final String STATUS = "Status";
+    private static final String PRICES = "Prices";
+
+
+    }
+
+
+
+
---
+++ src/main/java/com/carrot_cake/ecommerce/constants/ProductConstants.java
@@ -0,0 +1,26 @@
+package com.carrot_cake.ecommerce.constants;
+
+public class ProductConstants
+{
+    public static final String MESSAGE_417_DELETE = null;
+    public static final String MESSAGE_200 = null;
+    public static final String STATUS_417 = null;
+    public static final String STATUS_201 = null;
+    public static final String STATUS_200 = null ;
+
+    private ProductConstants()
+    {
+    }
+
+    public static final String PRODUCTS = "Products";
+    public static final String  STOCKS = "Stocks";
+    public static final String ORDERS = "Orders";
+
+    public static final String PRODUCTAVAILABILITY = "ProductAvailability";
+
+
+    public static final String PRODUCLIFECYCLE = "ProductLifeCyscel";
+}
+
+
+
