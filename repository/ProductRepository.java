c944c29b0ba1d585d0e1f7965f7b7318b2125926
GitHUBProject26 <eugenemaz707@gmail.com>
2024-05-25 09:52:18 +0300

    Add files via upload

---
+++ repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.carrot_cake.ecommerce.repository;
+
+import com.carrot_cake.ecommerce.*;
+import com.carrot_cake.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>
+{
+
+    Optional<Product> findByMobileNumber(String mobileNumber);
+
+
+}
